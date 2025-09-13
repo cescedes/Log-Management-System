56143412deb09a612417981ab098ed795a74589c
aybüke <77280855+cescedes@users.noreply.github.com>
2025-09-13 20:30:44 +0200

    Update directory_operations.go

--- directory_operations.go
+++ directory_operations.go
@@ -1,79 +1,67 @@
 package main
 
 import (
-	"fmt"
-	"io/ioutil"
-  "os"
+    "fmt"
+    "io/ioutil"
+    "os"
 )
 
 func createLogDirectory() {
-	fmt.Println("Enter the name of the log directory you'd like to create:")
-	var dirName string
-	fmt.Scanln(&dirName)
+    fmt.Println("Enter the name of the log directory you'd like to create:")
+    var dirName string
+    fmt.Scanln(&dirName)
 
-	err:= os.Mkdir(dirName, 0755)
-
-  if err != nil {
-    fmt.Println(err)
-    return
-  }
-  fmt.Printf("Log directory '%s' created successfully.\n", dirName)
-
+    err := os.Mkdir(dirName, 0755)
+    if err != nil {
+        fmt.Println(err)
+        return
+    }
+    fmt.Printf("Log directory '%s' created successfully.\n", dirName)
 }
 
 func changeLogDirectory() {
-	cwd := getWorkingDirectory()
-	fmt.Println("Current Directory:", cwd)
+    cwd := getWorkingDirectory()
+    fmt.Println("Current Directory:", cwd)
 
-	fmt.Print("Enter the name of the log directory you want to change to: ")
-	var newDirName string
-	fmt.Scanln(&newDirName)
+    fmt.Print("Enter the name of the log directory you want to change to: ")
+    var newDirName string
+    fmt.Scanln(&newDirName)
 
-	err := os.Chdir(newDirName)
-  if err != nil {
-    fmt.Println(err)
-    return
-  }
-
-
-	newCwd := getWorkingDirectory()
-	fmt.Println("Directory changed successfully. Current Directory:", newCwd)
-
-  cwd, err := os.Getwd()
-  if err != nil {
-    fmt.Println(err)
-    return ""
-  }
-  return title
-
+    err := os.Chdir(newDirName)
+    if err != nil {
+        fmt.Println(err)
+        return
+    }
+    newCwd := getWorkingDirectory()
+    fmt.Println("Directory changed successfully. Current Directory:", newCwd)
 }
 
 func printWorkingDirectory() {
-	cwd := getWorkingDirectory()
-	fmt.Println("Current Working Directory:", cwd)
+    cwd := getWorkingDirectory()
+    fmt.Println("Current Working Directory:", cwd)
 }
 
 func listDirectoryContents() {
-	cwd := getWorkingDirectory()
-	fmt.Println("Current Working Directory:", cwd)
+    cwd := getWorkingDirectory()
+    fmt.Println("Current Working Directory:", cwd)
 
-	files, err := ioutil.ReadDir(cwd)
-	if err != nil {
-		fmt.Println("Error reading directory contents:", err)
-		return
-	}
+    files, err := ioutil.ReadDir(cwd)
+    if err != nil {
+        fmt.Println("Error reading directory contents:", err)
+        return
+    }
 
-	fmt.Println("Directory Contents:")
-	for _, file := range files {
-		fmt.Println(file.Name())
-	}
+    fmt.Println("Directory Contents:")
+    for _, file := range files {
+        fmt.Println(file.Name())
+    }
 }
 
 func getWorkingDirectory() string {
-	cwd, err := os.Getwd()
-	if err != nil {
-		fmt.Println(err)
-		return ""
-	}
-	return cwd
+    cwd, err := os.Getwd()
+    if err != nil {
+        fmt.Println(err)
+        return ""
+    }
+    return cwd
 }
